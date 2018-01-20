66df6f90391582ac597b63009e4e58336bcde724
Zach Martinez <martinez.zach.m@gmail.com>
2018-01-19 20:06:39 -0800

    Add makeColumn function

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const gridContainer = document.querySelector(".grid-container");
-const grid = createGrid(100);
 
-gridContainer.appendChild(grid);
+function makeGrid (n) {
+    for
+}
 
-function createGrid () {
+function makeColumn (n) {
+    const column = document.createElement("div");
+    column.classList.add("column");
 
+    const cell = document.createElement("div");
+    const cellSize = gridContainer.offsetWidth / n;
+    cell.classList.add("cell");
+    cell.style.width = `${cellSize}px`;
+    cell.style.height = `${cellSize}px`;
+
+    for (i = 0; i < n; i++) {
+        column.appendChild(cell.cloneNode());
+        console.log(column.childElementCount);
+    }
+
+    gridContainer.appendChild(column);
 }
