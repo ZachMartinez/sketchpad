92cd45c083bdf1be64adc71c79fa4385f7a4c7cf
Zach Martinez <martinez.zach.m@gmail.com>
2018-01-25 17:54:43 -0800

    Major commit, change combine functionality of makeCol and drawCol into one function

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-
-
-
-
-function drawRow () {
-    const grid = document.querySelector('.grid');
-
-    const cell = document.createElement('div');
-          cell.classList.add('cell');
-
-    for (i = 0; i < 16; i++) {
-        grid.appendChild(cell.cloneNode());
-    }
-}
+const grid = document.querySelector(".grid");
 
 function drawCol () {    /* Draws a column of cells in the grid */
-    const grid = document.querySelector('.grid'); /* Get grid element*/
+    const grid = document.querySelector('.grid'); 
 
-    const cell = document.createElement('div'); /* Create cell */
+    const cell = document.createElement('div'); 
           cell.classList.add('cell');
 
     for (i = 0; i < 16; i++) {
         const newCell = cell.cloneNode(); /* Make each cell appear one grid-row below previous */
-              newCell.style.gridRow = i + 1;
+              newCell.style.gridRow = i + 1; /* Grid rows start at 1 */
 
         grid.appendChild(newCell);
     }
