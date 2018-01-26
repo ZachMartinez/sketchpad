0039fe3f3b81642c457fe99964ef3b08cd618caa
Zach Martinez <martinez.zach.m@gmail.com>
2018-01-25 20:54:24 -0800

    Merge drawCol function with drawGrid

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const grid = document.querySelector(".grid");
+const grid = document.querySelector('.grid'); 
 
-function drawCol () {    /* Draws a column of cells in the grid */
-    const grid = document.querySelector('.grid'); 
-
+const drawGrid = () => {
     const cell = document.createElement('div'); 
           cell.classList.add('cell');
 
-    for (i = 0; i < 16; i++) {
-        const newCell = cell.cloneNode(); /* Make each cell appear one grid-row below previous */
-              newCell.style.gridRow = i + 1; /* Grid rows start at 1 */
-
-        grid.appendChild(newCell);
+    for (j = 0; j < 16; j++) { /* Repeat columns */ 
+        for (i = 0; i < 16; i++) {
+            const newCell = cell.cloneNode(); /* Make each cell appear one grid-row below previous */
+                  newCell.style.gridRow = i + 1; /* Grid rows start at 1 */
+    
+            grid.appendChild(newCell);
+        }
     }
 }
+
